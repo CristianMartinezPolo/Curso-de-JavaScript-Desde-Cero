a66c94d71674d3ceff60df5d0ca6064148414cc8
CristianMartinezPolo <cristianmartinez9519@outlook.com>
2024-04-09 11:02:29 -0500

    Paso por referencia-Tipos de dato complejo

---
+++ clase-paso-por-referencia.js
@@ -0,0 +1,14 @@
+// Paso por referencia -Tipo de dato complejo
+
+let frutas = {naranja: 'naran'}
+let vegetales = frutas
+
+vegetales.naranja = 'Brocoli'
+
+console.log(frutas, vegetales)
+
+let ropa = {blusa: 'blus'}
+
+ropa.pantalon = 'pant'
+
+console.log(ropa)
