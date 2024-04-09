4eafdd427b8c4bd9ff12f07d51835c64c469f32a
CristianMartinezPolo <cristianmartinez9519@outlook.com>
2024-04-08 22:02:59 -0500

    Variables y constantes

---
+++ clase-variables.js
@@ -0,0 +1,14 @@
+let cajaDeAndy = "Woody"
+console.log(cajaDeAndy)
+
+// Lo NO permitido
+let c = "Woody" //nombres de variables que nos describen su contenido
+let cda = "Woody" //nombres de variables que nos describen su contenido
+let pcAndy = "Woody" //nombres de variables que nos describen su contenido
+
+// Lo permitido
+let primerTrasteoDeAndy = "Woody"
+let urlDelUsuario = "https://www.google.com"
+let idDelUsuario = "123465789"
+
+const pi = 3.1416
